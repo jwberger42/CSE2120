1ae74ee4fb18c1a5f9c2f921a21d6433dbdd592f
John Berger <jwberger@shaw.ca>
2017-04-04 09:03:31 -0600

    Add project files.

---
+++ 2120Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2120Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
