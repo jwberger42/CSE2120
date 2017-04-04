23205805d2476f2158ff3f3161b05545550da297
John Berger <jwberger@shaw.ca>
2017-04-04 10:03:31 -0600

    Done 1.3, 2

--- 2120Project/Program.cs
+++ 2120Project/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _2120Project
+namespace HelloWorld
 {
-    class Program
+    public class Hello
     {
-        static void Main(string[] args)
+        private static List<String> list = new List<String>();
+        private static String choice;
+        public static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            makeChoice();
+            if (choice.Contains("yes"))
+            {
+                makeChoice();
+            }
+            if (choice.Contains("no"))
+            {
+                foreach (String i in list)
+                {
+                    foreach (String o in i.Split(' '))
+                    {
+                        Console.WriteLine(o);
+                    }
+                }
+            }
+            Console.Read();
+        }
+        public static void makeChoice()
+        {
+            Console.WriteLine("Enter a sentence:");
+            list.Add(Console.ReadLine());
+            Console.WriteLine("Do you want to enter another sentence?");
+            choice = Console.ReadLine();
+            choice = choice.ToLower();
         }
     }
 }
